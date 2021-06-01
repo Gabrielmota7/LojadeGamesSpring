7849b352a9e47cf9ffd0f759f7fecd1c314b3c74
Gabrielmota7 <82525773+Gabrielmota7@users.noreply.github.com>
2021-06-01 17:19:10 -0300

    Loja de Games

---
+++ LojadeGames/src/main/java/org/generation/lojadegames/model/Categoria.java
@@ -0,0 +1,60 @@
+package org.generation.lojadegames.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min=5, max = 100)
+	private String Tipo;
+	
+	@OneToMany
+	@JsonIgnoreProperties("categoria")
+	private List <Produto> Plataforma;
+
+	public List<Produto> getProduto() {
+		return Plataforma;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		Plataforma = produto;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return Tipo;
+	}
+
+	public void setTipo(String tipo) {
+		Tipo = tipo;
+	}
+
+	
+
+}
